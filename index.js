dcb708c3945c020f177cee4a1204d87aba71d88f
周泽飞 <zefei.zhou@tongdun.net>
2020-10-23 18:23:42 +0800

    增加运行脚本

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var execSync = require("child_process").execSync;
 var fs = require("fs");
+var path = require("path");
 
 // 格式化时间
 var dateFormat = function dateFormat(fmt, date) {
@@ -18,7 +19,7 @@ var dateFormat = function dateFormat(fmt, date) {
 	for (var k in opt) {
 		ret = new RegExp("(" + k + ")").exec(fmt);
 		if (ret) {
-			fmt = fmt.replace(ret[1], ret[1].length == 1 ? opt[k] : opt[k].padStart(ret[1].length, "0"));
+			fmt = fmt.replace(ret[1], ret[1].length === 1 ? opt[k] : opt[k].padStart(ret[1].length, "0"));
 		};
 	};
 	return fmt;
@@ -34,24 +35,62 @@ BranchPlugin.prototype.apply = function (compiler) {
 
 	compiler.hooks.afterEmit.tap("branch-plugin", function () {
 		// 发布信息
-		var branchInfo = execSync("git name-rev --name-only HEAD", { "encoding": "utf8" });
-		var userName = execSync("git config user.name", { "encoding": "utf8" });
-		var mailName = execSync("git config user.email", { "encoding": "utf8" });
-		var publishTime = dateFormat("YYYY-mm-dd HH:MM:SS", new Date());
-		var publishStr = "\u8D44\u6E90\u5305\u63D0\u4F9B\u8005:" + userName + "\u90AE\u7BB1:" + mailName + "\u751F\u6210\u65E5\u671F:" + publishTime + "\n\u53D1\u5E03\u5206\u652F:" + branchInfo + new Array(80).join("*") + "\n";
+		var branchInfo = "\n",
+		    commandInfo = "\n",
+		    userName = "\n",
+		    mailName = "\n",
+		    publishTime = "\n",
+		    publishStr = "\n";
+
+		try {
+			commandInfo = process.env["npm_lifecycle_script"].toString().trim();
+		} catch (e) {}
+		try {
+			branchInfo = execSync("git rev-parse --abbrev-ref HEAD");
+		} catch (e) {}
+		try {
+			userName = execSync("git config user.name");
+		} catch (e) {}
+		try {
+			mailName = execSync("git config user.email");
+		} catch (e) {}
+		try {
+			publishTime = dateFormat("YYYY-mm-dd HH:MM:SS", new Date());
+		} catch (e) {}
+
+		publishStr = "\u8D44\u6E90\u5305\u63D0\u4F9B\u8005:" + userName + "\u6267\u884C\u811A\u672C:" + commandInfo + "\n\u90AE\u7BB1:" + mailName + "\u751F\u6210\u65E5\u671F:" + publishTime + "\n\u53D1\u5E03\u5206\u652F:" + branchInfo + new Array(80).join("*") + "\n";
 
 		// 最后一次提交记录信息
-		var commit = execSync("git show -s --format=%H").toString().trim(); // 姓名
-		var name = execSync("git show -s --format=%cn").toString().trim(); // 姓名
-		var email = execSync("git show -s --format=%ce").toString().trim(); // 邮箱
-		var date = new Date(execSync("git show -s --format=%cd").toString()); // 日期
-		var message = execSync("git show -s --format=%s").toString().trim(); // 说明
-		var versionStr = "\u6700\u8FD1\u4E00\u6B21\u63D0\u4EA4:\ngit:" + commit + "\n\u4F5C\u8005:" + name + "<" + email + ">\n\u65E5\u671F:" + (date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes()) + "\n\u8BF4\u660E:" + message + "\n" + new Array(80).join("*") + "\n";
+		var commit = "",
+		    name = "",
+		    email = "",
+		    date = "",
+		    message = "",
+		    versionStr = "";
 
+		try {
+			commit = execSync("git show -s --format=%H").toString().trim();
+		} catch (e) {}
+		try {
+			name = execSync("git show -s --format=%cn").toString().trim();
+		} catch (e) {}
+		try {
+			email = execSync("git show -s --format=%ce").toString().trim();
+		} catch (e) {}
+		try {
+			date = new Date(execSync("git show -s --format=%cd").toString());
+		} catch (e) {}
+		try {
+			message = execSync("git show -s --format=%s").toString().trim(); // 说明
+		} catch (e) {}
+
+		if (commit || name || email || date || message) {
+			versionStr = "\u6700\u8FD1\u4E00\u6B21\u63D0\u4EA4:\ngit:" + commit + "\n\u4F5C\u8005:" + name + "<" + email + ">\n\u65E5\u671F:" + (date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes()) + "\n\u8BF4\u660E:" + message + "\n" + new Array(80).join("*") + "\n";
+		}
 		// 写入目录
 
 		var _ref = compiler.options.output || {},
-		    path = _ref.path;
+		    pathUrl = _ref.path;
 
 		var _ref2 = _this.options || {},
 		    filename = _ref2.filename;
@@ -59,11 +98,13 @@ BranchPlugin.prototype.apply = function (compiler) {
 		if (!filename.startsWith("/")) {
 			filename = "/" + filename;
 		}
-		fs.writeFileSync(path + filename, publishStr + versionStr, function (err) {
-			if (err) {
-				console.warn(err);
-			}
-		});
+		try {
+			fs.writeFileSync(path.join(pathUrl + filename), publishStr + versionStr, {
+				encoding: "utf-8",
+				mode: 438 /* =0666*/
+				, flag: "w"
+			});
+		} catch (e) {}
 	});
 };
 
--- index_src.js
+++ index_src.js
@@ -1,5 +1,6 @@
 const execSync = require("child_process").execSync;
 const fs = require("fs");
+const path = require("path");
 
 // 格式化时间
 const dateFormat = (fmt, date) => {
@@ -16,7 +17,7 @@ const dateFormat = (fmt, date) => {
 	for (let k in opt) {
 		ret = new RegExp("(" + k + ")").exec(fmt);
 		if (ret) {
-			fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")));
+			fmt = fmt.replace(ret[1], (ret[1].length === 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")));
 		};
 	};
 	return fmt;
@@ -30,31 +31,60 @@ const BranchPlugin = function(options) {
 BranchPlugin.prototype.apply = function(compiler) {
 	compiler.hooks.afterEmit.tap("branch-plugin", ()=>{
 		// 发布信息
-		const branchInfo = execSync("git name-rev --name-only HEAD", { "encoding": "utf8" });
-		const userName = execSync("git config user.name", { "encoding": "utf8" });
-		const mailName = execSync("git config user.email", { "encoding": "utf8" });
-		const publishTime = dateFormat("YYYY-mm-dd HH:MM:SS", new Date());
-		const publishStr = `资源包提供者:${userName}邮箱:${mailName}生成日期:${publishTime}\n发布分支:${branchInfo}${new Array(80).join("*")}\n`;
+		let [branchInfo, commandInfo, userName, mailName, publishTime, publishStr] = ["\n", "\n", "\n", "\n", "\n", "\n"];
+		try {
+			commandInfo = process.env["npm_lifecycle_script"].toString().trim();
+		} catch (e) {}
+		try {
+			branchInfo = execSync("git rev-parse --abbrev-ref HEAD");
+		} catch (e) {}
+		try {
+			userName = execSync("git config user.name");
+		} catch (e) {}
+		try {
+			mailName = execSync("git config user.email");
+		} catch (e) {}
+		try {
+			publishTime = dateFormat("YYYY-mm-dd HH:MM:SS", new Date());
+		} catch (e) {}
+
+		publishStr = `资源包提供者:${userName}执行脚本:${commandInfo}\n邮箱:${mailName}生成日期:${publishTime}\n发布分支:${branchInfo}${new Array(80).join("*")}\n`;
 
 		// 最后一次提交记录信息
-		let commit = execSync("git show -s --format=%H").toString().trim(); // 姓名
-		const name = execSync("git show -s --format=%cn").toString().trim(); // 姓名
-		const email = execSync("git show -s --format=%ce").toString().trim(); // 邮箱
-		const date = new Date(execSync("git show -s --format=%cd").toString()); // 日期
-		const message = execSync("git show -s --format=%s").toString().trim(); // 说明
-		const versionStr = `最近一次提交:\ngit:${commit}\n作者:${name}<${email}>\n日期:${date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes()}\n说明:${message}\n${new Array(80).join("*")}\n`;
+		let [commit, name, email, date, message, versionStr] = ["", "", "", "", "", ""];
+		try {
+			commit = execSync("git show -s --format=%H").toString().trim();
+		} catch (e) {}
+		try {
+			name = execSync("git show -s --format=%cn").toString().trim();
+		} catch (e) {}
+		try {
+			email = execSync("git show -s --format=%ce").toString().trim();
+		} catch (e) {}
+		try {
+			date = new Date(execSync("git show -s --format=%cd").toString());
+		} catch (e) {}
+		try {
+			message = execSync("git show -s --format=%s").toString().trim(); // 说明
+		} catch (e) {}
 
+		if (commit || name || email || date || message) {
+			versionStr = `最近一次提交:\ngit:${commit}\n作者:${name}<${email}>\n日期:${date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes()}\n说明:${message}\n${new Array(80).join("*")}\n`;
+		}
 		// 写入目录
-		const { path } = compiler.options.output || {};
+		let { path: pathUrl } = compiler.options.output || {};
 		let { filename } = this.options || {};
 		if (!filename.startsWith("/")) {
 			filename = "/" + filename;
 		}
-		fs.writeFileSync(path + filename, publishStr + versionStr, (err)=>{
-			if (err) {
-				console.warn(err);
-			}
-		});
+		try {
+			fs.writeFileSync(path.join(pathUrl + filename), publishStr + versionStr, {
+				encoding: "utf-8",
+				mode: 438 /* =0666*/,
+				flag: "w"
+			});
+		} catch (e) {
+		}
 	});
 };
 
